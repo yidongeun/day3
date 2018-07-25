6cb0f2e217f8a50b86acf5d115da2876b3cdebfd
G7yi <erun7meu1@daum.net>
2018-07-25 17:50:35 +0900

    list

---
+++ dict.py
@@ -0,0 +1,16 @@
+person = {'name': 'G7yi', 'address': 'dongeun','email': 'erun7meu1@daum.net'}
+print(person)
+print(person['name'])
+print(person['email'])
+print(person.items())
+for key, value in person.items():
+    print("\nkey : " + key)
+    print("Value : " + value)
+
+Key \ 'name'
+Value \ 'G7yi'
+    print("\nkey: " + key", "G7yi:"+ value)
+    
+
+
+
--- list.py
+++ list.py
@@ -1,2 +1,8 @@
-fruits = [ 'apple', 'banana, 'watermelon']
+fruits = [ 'apple', 'banana', 'watermelon']
 print(fruits)
+print(fruits[0])
+fruits.append('orange')
+print(fruits)
+del fruits[1]
+print(fruits)
+
