5d8e7f2a07a291cd9a410e383dbceda07b2d3808
G7yi <erun7meu1@daum.net>
2018-07-26 14:30:03 +0900

    함수 매개변수와 return

--- function.py
+++ function.py
@@ -5,9 +5,16 @@
 # print(n1-n2)
 # print(n1*n2)
 
-def hello(name):
+def hello(name='G7yi', age='10'):
     print("Hello, " + name)
+    print(age + "years old")
 
-typing = input("What is your namr? ")
-hello(typing)
-print(name)
+# typing = input("What is your namr? ")
+# hello(typing)
+
+name = input("Your Name: ")
+age = input("How old are you? ")
+hello(name, age)
+hello(name)
+hello()
+hello(age='60', name='artrun')
---
+++ function2.py
@@ -0,0 +1,19 @@
+# def sum(n1, n2):
+#     print("sum 함수 : ",n1, n2)
+#     return n1+n2
+
+#     num1 = 10:
+#     num2 = 20:
+#     result = sum(nu1,num2) + sum(1,2)
+#     print(result)
+
+    # print(sum(1,2))
+    # print("sum 함수 밖 : ", num1.num2)
+
+ def cart(fruits):
+     print("장바구니 안에 다음과 같은 과일이 있습니다")
+     for fruit  in fruits:
+         print(fruit)
+
+fruits = ['apple', 'banana', 'orange']
+cart(fruits)
